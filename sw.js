5802bb02cbbe3925273e3d7af56c3c322f00ed21
elfsiz <diem292513@gmail.com>
2026-04-29 10:07:57 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,33 +1,18 @@
-const CACHE = 'boss-timer-v1';
-const ASSETS = ['./boss-timer.html', './manifest.json'];
-
+// 캐시 사용 안 함 - 항상 최신 파일 사용
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
-  );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
+  // 기존 캐시 전부 삭제
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+      Promise.all(keys.map(k => caches.delete(k)))
     ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', e => {
-  // Firebase 요청은 캐시하지 않음 (항상 네트워크)
-  if(e.request.url.includes('firebase') || e.request.url.includes('gstatic')){
-    e.respondWith(fetch(e.request));
-    return;
-  }
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
-      if(res && res.status === 200 && res.type === 'basic'){
-        const clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-      }
-      return res;
-    }))
-  );
+  // 모든 요청을 항상 네트워크에서 가져옴
+  e.respondWith(fetch(e.request));
 });
